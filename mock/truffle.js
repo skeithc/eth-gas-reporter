3d2db333512cb5d1a36ae05a84ab97546e4c876a
Skeith <github@skeith.dev>
2023-09-03 18:37:11 +0800

    Remove dependency on ethereum-cryptography and use ethers instead

--- mock/truffle.js
+++ mock/truffle.js
@@ -3,10 +3,27 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*",
+      websockets: process.env.TEST === "integration" ? true : false
     }
   },
   mocha: {
-    reporter: "eth-gas-reporter"
+    reporter: "eth-gas-reporter",
+    reporterOptions: {
+      currency: "chf",
+      token: "ETH",
+      coinmarketcap: process.env.COINMARKETCAP_API_KEY || null,
+      gasPriceApi:
+        "https://api.etherscan.io/api?module=proxy&action=eth_gasPrice",
+      onlyCalledMethods: false,
+      noColors: true,
+      rst: true,
+      rstTitle: "Gas Usage",
+      showTimeSpent: true,
+      excludeContracts: ["Migrations"],
+      proxyResolver: "EtherRouter",
+      codechecks: true,
+      showMethodSig: true
+    }
   }
 };
